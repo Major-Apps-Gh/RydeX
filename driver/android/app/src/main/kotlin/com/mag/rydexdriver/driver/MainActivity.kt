60b7feb4fd123b7438440d488b1ec31c668221d1
daveotengo <30934250+daveotengo@users.noreply.github.com>
2023-05-14 15:28:00 +0000

    first commit

---
+++ driver/android/app/src/main/kotlin/com/mag/rydexdriver/driver/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mag.rydexdriver.driver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ passenger/android/app/src/main/kotlin/com/mag/rydexpassenger/passenger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mag.rydexpassenger.passenger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
